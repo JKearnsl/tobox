4a6fad1da37ea8da6066e3eace06ec4254482e60
JKearnsl <boyarshinovn@gmail.com>
2024-03-27 22:20:51 +0300

    initial

---
+++ panel/src/App.tsx
@@ -0,0 +1,14 @@
+import {Auth} from "@/components/auth.tsx";
+
+function App() {
+
+  return (
+      <>
+          <div>
+              <Auth />
+          </div>
+      </>
+  )
+}
+
+export default App

609c39606e5e91639d2fa67341fe21fe575ca600
JKearnsl <boyarshinovn@gmail.com>
2024-07-21 23:53:24 +0300

    update: changed app path

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: 'index.html',
+  },
+  root: 'src/panel',
+  publicDir: 'assets',
+  build: {
+    outDir: '../../web',
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src/panel"),
+      "/src/panel": path.resolve(__dirname, "./src/panel"),
     },
   },
-})
+});

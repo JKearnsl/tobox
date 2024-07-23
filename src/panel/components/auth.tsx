ef19afaae69bbfadc961b55ed1fbb378a146473b
JKearnsl <boyarshinovn@gmail.com>
2024-07-23 20:54:46 +0300

    update: Now the login page is more expressive

--- src/panel/components/auth.tsx
+++ src/panel/components/auth.tsx
@@ -1,57 +1,117 @@
-import { Button } from "./ui/button";
-import { Input } from "./ui/input";
-import { Label } from "./ui/label";
-import { Link } from "lucide-react";
+
+import {Button} from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import {Input} from "@/components/ui/input"
+import {Label} from "@/components/ui/label"
+import {useState} from "react";
+
+function BasicLoginForm() {
+  return (
+      <>
+        <div className="grid gap-2">
+          <Label htmlFor="username">Username</Label>
+          <Input
+              id="username"
+              type="username"
+              placeholder="supauser"
+              required
+          />
+        </div>
+        <div className="grid gap-2">
+          <div className="flex items-center">
+            <Label htmlFor="password">Password</Label>
+          </div>
+          <Input id="password" type="password" required/>
+        </div>
+      </>
+  )
+}
+
+
+function URILoginForm() {
+  return (
+      <>
+        <div className="grid gap-2">
+          <Label htmlFor="uri">URI</Label>
+          <Input
+              id="uri"
+              type="text"
+              placeholder="https://supauser:password@node.com"
+                  required
+              />
+          </div>
+      </>
+  )
+}
+
 
 export function Auth() {
+  const [isBasicForm, setIsBasicForm] = useState(true);
+
+  const toggleForm = () => setIsBasicForm(!isBasicForm);
+
   return (
-    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
-      <div className="flex items-center justify-center py-12">
-        <div className="mx-auto grid w-[350px] gap-6">
-          <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold">Login</h1>
-            <p className="text-balance text-muted-foreground">
-              Enter your email below to login to your account
-            </p>
-          </div>
-          <div className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <Card className="mx-auto max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>
+            {
+                isBasicForm ?
+                    "Enter your username below to login to your account" :
+                    "Enter your URI below to login to your account"
+              }
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <div className="grid gap-4">
+              {isBasicForm ? <BasicLoginForm/> : <URILoginForm/>}
+
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+
+              <Button onClick={toggleForm} variant="outline" className="w-full">
+                {isBasicForm ? "Login with URI" : "Login with username"}
+              </Button>
+
             </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <Link
-                  href="/forgot-password"
-                  className="ml-auto inline-block text-sm underline"
-                >
-                  Forgot your password?
-                </Link>
-              </div>
-              <Input id="password" type="password" required />
+            <div className="mt-4 text-center text-sm">
+
+                <AlertDialog>
+                    <AlertDialogTrigger>How to get account</AlertDialogTrigger>
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>How to get account?</AlertDialogTitle>
+                            <AlertDialogDescription>
+                                Text stub
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <AlertDialogAction>Ok</AlertDialogAction>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
             </div>
-            <Button type="submit" className="w-full">
-              Login
-            </Button>
-            <Button variant="outline" className="w-full">
-              Login with Google
-            </Button>
-          </div>
-          <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link href="#" className="underline">
-              Sign up
-            </Link>
-          </div>
-        </div>
+          </CardContent>
+        </Card>
       </div>
-      <div className="hidden bg-muted lg:block"></div>
-    </div>
   );
 }

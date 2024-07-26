65c0d5c0d0b21261d7845636b6bf7833f7f9ba43
JKearnsl <boyarshinovn@gmail.com>
2024-07-26 12:16:22 +0300

    update: added path, hash, size, content_type, metadata fields

--- src/tobox/domain/models/object.rs
+++ src/tobox/domain/models/object.rs
@@ -1,6 +1,6 @@
+use std::collections::HashMap;
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
-use strum_macros::{Display, EnumString};
 use uuid::Uuid;
 
 pub type ObjectId = Uuid;
@@ -9,6 +9,11 @@ pub type ObjectId = Uuid;
 pub struct Object {
     pub id: ObjectId,
     pub name: String,
+    pub path: String,
+    pub hash: String,
+    pub size: u64,
+    pub content_type: String,
+    pub metadata: HashMap<String, String>,
     pub created_at: DateTime<Utc>,
-    pub updated_at: Option<DateTime<Utc>>,
+    pub updated_at: Option<DateTime<Utc>>
 }

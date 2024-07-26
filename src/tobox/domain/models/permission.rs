e1fdb553b940263c15be3773cfe47fd6c6d762ac
JKearnsl <boyarshinovn@gmail.com>
2024-07-26 11:51:52 +0300

    update: added box/object perms

--- src/tobox/domain/models/permission.rs
+++ src/tobox/domain/models/permission.rs
@@ -1,4 +1,5 @@
 use strum_macros::{Display, EnumIter};
+use crate::domain::models::r#box::BoxId;
 
 #[derive(Display, EnumIter)]
 pub enum Permission {
@@ -19,4 +20,28 @@ pub enum Permission {
     
     GetPermission,
     LinkRolePermission,
+    
+    GetBox,
+    CreateBox,
+    UpdateBox,
+    DeleteBox,
+    
+    #[strum(serialize = "UpdateSpecificBox({0})")]
+    UpdateSpecificBox(BoxId),
+    #[strum(serialize = "DeleteSpecificBox({0})")]
+    DeleteSpecificBox(BoxId),
+    
+    GetObject,
+    CreateObject,
+    UpdateObject,
+    DeleteObject,
+
+    #[strum(serialize = "GetSpecificObject({0})")]
+    GetSpecificObject(BoxId),
+    #[strum(serialize = "CreateSpecificObject({0})")]
+    CreateSpecificObject(BoxId),
+    #[strum(serialize = "UpdateSpecificObject({0})")]
+    UpdateSpecificObject(BoxId),
+    #[strum(serialize = "DeleteSpecificObject({0})")]
+    DeleteSpecificObject(BoxId)
 }

f7a85de33fdc1e308c013839bac21ada43a6b913
JKearnsl <boyarshinovn@gmail.com>
2024-07-27 06:42:55 +0300

    update: added ConfigManager

--- src/tobox/config.rs
+++ src/tobox/config.rs
@@ -1,7 +1,7 @@
 use std::fs::{File, OpenOptions};
 use std::io::Read;
 use std::io::Write;
-
+use std::sync::RwLock;
 use serde::{Deserialize, Serialize};
 
 
@@ -59,76 +59,126 @@ pub struct Tls {
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Config {
-    config_path: String,
     pub log_level: Option<String>,
     pub panel: Option<PanelConfig>,
     pub node: Option<NodeConfig>,
 }
 
-impl Config {
-    pub fn new(config_path: &str) -> Result<Self, String> {
-        if !std::path::Path::new(config_path).exists() {
-            let default_config = Self {
-                config_path: config_path.to_string(),
-                log_level: Some("info".to_string()),
-                panel: Some(PanelConfig {
-                    host: "127.0.0.1".to_string(),
-                    port: 8080,
-                    tls: None
-                }),
-                node: Some(NodeConfig {
-                    host: "127.0.0.1".to_string(),
-                    port: 3030,
-                    tls: None,
-                    workers: None,
-                    is_intermediate: false,
-                    disk: DiskConfig {
-                        mode: ClusterMode::Combined,
-                        volumes: vec![]
-                    },
-                    cluster: None,
-                    credentials: None
-                })
-            };
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            log_level: Some("info".to_string()),
+            panel: Some(PanelConfig {
+                host: "127.0.0.1".to_string(),
+                port: 8080,
+                tls: None
+            }),
+            node: Some(NodeConfig {
+                host: "127.0.0.1".to_string(),
+                port: 3030,
+                tls: None,
+                workers: None,
+                is_intermediate: false,
+                disk: DiskConfig {
+                    mode: ClusterMode::Combined,
+                    volumes: vec![]
+                },
+                cluster: None,
+                credentials: None
+            })
+        }
+    }
+}
 
+pub struct ConfigManager {
+    config: RwLock<Config>,
+    path: String,
+}
+
+impl ConfigManager {
+    pub fn new(config_path: &str) -> Self {
+        if !std::path::Path::new(config_path).exists() {
             match OpenOptions::new().write(true).create_new(true).open(config_path) {
                 Ok(mut file) => {
-                    if let Err(error) = writeln!(file, "{}", serde_yaml::to_string(&default_config).unwrap()) {
-                        return Err(format!("Failed to write to {} -> {}", config_path, error));
+                    if let Err(error) = writeln!(file, "{}", serde_yaml::to_string(&Config::default()).unwrap()) {
+                        log::error!("Failed to write to {}: {}", config_path, error);
+                        std::process::exit(1);
                     }
                 },
-                Err(error) => return Err(format!("Failed to create {} -> {}", config_path, error)),
+                Err(error) => {
+                    log::error!("Failed to create {}: {}", config_path, error);
+                    std::process::exit(1);
+                },
             };
         }
-        
+
         let mut file = match File::open(config_path) {
             Ok(file) => file,
-            Err(error) => return Err(format!("Failed to open {} -> {}", config_path, error)),
+            Err(error) => { 
+                log::error!("Failed to open {}: {}", config_path, error);
+                std::process::exit(1);
+            }
         };
         let mut contents = String::new();
         match file.read_to_string(&mut contents) {
             Ok(_) => (),
-            Err(error) => return Err(format!("Failed to read {} -> {}", config_path, error)),
+            Err(error) => { 
+                log::error!("Failed to read from {}: {}", config_path, error);
+                std::process::exit(1);
+            }
         };
 
         let config = match serde_yaml::from_str(&contents) {
             Ok(config) => config,
             Err(error) => {
-                return Err(format!("Failed to parse {} -> {}", config_path, error));
+                log::error!("Failed to parse {}: {}", config_path, error);
+                std::process::exit(1);
             },
         };
-        Ok(config)
+        
+        Self {
+            config: RwLock::new(config),
+            path: config_path.to_string(),
+        }
     }
     
-    pub fn save(&self, config: Config) -> Result<(), String> {
-        match OpenOptions::new().write(true).truncate(true).open(&config.config_path) {
-            Ok(mut file) => {
-                if let Err(error) = writeln!(file, "{}", serde_yaml::to_string(&config).unwrap()) {
-                    return Err(format!("Failed to write to {} -> {}", config.config_path, error));
-                }
-            },
-            Err(error) => return Err(format!("Failed to open {} -> {}", config.config_path, error)),
+    pub fn get(&self) -> Config {
+        self.config.read().unwrap().clone()
+    }
+    
+    pub fn set(&self, config: Config) {
+        self.config.write().unwrap().clone_from(&config);
+    }
+    
+    pub fn save(&self) -> Result<(), String> {
+        let config = self.config.read().unwrap().clone();
+        let mut file = match OpenOptions::new().write(true).truncate(true).open(&self.path) {
+            Ok(file) => file,
+            Err(error) => return Err(format!("Failed to open {}: {}", self.path, error)),
         };
+        if let Err(error) = writeln!(file, "{}", serde_yaml::to_string(&config).unwrap()) {
+            return Err(format!("Failed to write to {}: {}", self.path, error));
+        }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_config() {
+        let config = Config::new("test_config.yaml").unwrap();
+        assert_eq!(config.panel.is_some(), true);
+        assert_eq!(config.node.is_some(), true);
+    }
+
+    #[test]
+    fn test_config_manager() {
+        let config_manager = ConfigManager::new("test_config.yaml");
+        let config = config_manager.get();
+        assert_eq!(config.panel.is_some(), true);
+        assert_eq!(config.node.is_some(), true);
     }
 }

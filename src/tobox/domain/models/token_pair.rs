a2b67a9ba9b5aa4b8e4aae08d89912fa19c178b8
JKearnsl <boyarshinovn@gmail.com>
2024-07-27 03:27:37 +0300

    update: rename token -> token_pair

--- src/tobox/domain/models/token_pair.rs
+++ src/tobox/domain/models/token_pair.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 use crate::domain::models::user::UserId;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct Token {
+pub struct TokenPair {
     pub public: String,
     pub enc_private: String,
     pub user_id: UserId

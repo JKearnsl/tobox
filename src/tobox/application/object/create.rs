7d4f15313b631cebf9af6496f761f200af6b9493
JKearnsl <boyarshinovn@gmail.com>
2024-07-31 13:05:18 +0300

    update: impl object create

--- src/tobox/application/object/create.rs
+++ src/tobox/application/object/create.rs
@@ -1,91 +1,76 @@
-use std::collections::{BTreeMap, HashMap};
+use std::collections::HashMap;
 
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 
-use crate::application::common::email_sender::EmailSender;
+use crate::application::common::box_gateway::BoxReader;
 use crate::application::common::exceptions::{ApplicationError, ErrorContent};
-use crate::application::common::hasher::Hasher;
+use crate::application::common::file_storage_manager::FileStorageWriter;
 use crate::application::common::id_provider::IdProvider;
 use crate::application::common::interactor::Interactor;
-use crate::application::common::role_gateway::RoleGateway;
-use crate::application::common::user_gateway::UserGateway;
-use crate::config::Extra;
-use crate::domain::models::user::{UserId, UserState};
+use crate::application::common::object_gateway::ObjectGateway;
+use crate::domain::exceptions::DomainError;
+use crate::domain::models::file_stream::FileStream;
+use crate::domain::models::object::ObjectId;
 use crate::domain::services::access::AccessService;
-use crate::domain::services::user::UserService;
+use crate::domain::services::object::ObjectService;
 use crate::domain::services::validator::ValidatorService;
 
-#[derive(Debug, Deserialize)]
-pub struct CreateUserDTO {
-    pub username: String,
-    pub email: String,
-    pub password: String,
-    pub first_name: Option<String>,
-    pub last_name: Option<String>,
+#[derive(Deserialize)]
+pub struct CreateObjectDTO {
+    pub box_name: String,
+    pub name: Option<String>,
+    pub path: Option<String>,
+    pub file: dyn FileStream,
+    pub metadata: HashMap<String, String>
 }
 
-#[derive(Debug, Serialize)]
-pub struct CreateUserResultDTO{
-    id: UserId,
-    username: String,
-    email: String,
-    state: UserState,
-    first_name: Option<String>,
-    last_name: Option<String>,
+#[derive(Debug, Serialize)] 
+pub struct CreateObjectResultDTO{
+    pub id: ObjectId,
+    pub name: String,
+    pub path: String,
+    pub hash: String,
+    pub size: u64,
+    pub content_type: String,
+    pub metadata: HashMap<String, String>,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: Option<DateTime<Utc>>
 }
 
-pub struct CreateUser<'a> {
-    pub user_gateway: &'a dyn UserGateway,
-    pub role_gateway: &'a dyn RoleGateway,
-    pub email_sender: &'a dyn EmailSender,
-    pub user_service: &'a UserService,
-    pub password_hasher: &'a dyn Hasher,
+pub struct CreateObject<'a> {
+    pub box_reader: &'a dyn BoxReader,
+    pub file_storage_writer: &'a dyn FileStorageWriter,
+    pub object_gateway: &'a dyn ObjectGateway,
+    pub object_service: &'a ObjectService,
     pub validator: &'a ValidatorService,
     pub access_service: &'a AccessService,
     pub id_provider: Box<dyn IdProvider>,
-    pub extra: &'a Extra,
 }
 
-impl Interactor<CreateUserDTO, CreateUserResultDTO> for CreateUser<'_> {
-    async fn execute(&self, data: CreateUserDTO) -> Result<CreateUserResultDTO, ApplicationError> {
+impl Interactor<CreateObjectDTO, CreateObjectResultDTO> for CreateObject<'_> {
+    async fn execute(&self, data: CreateObjectDTO) -> Result<CreateObjectResultDTO, ApplicationError> {
         
-        match self.access_service.ensure_can_create_user(
-            &self.id_provider.permissions()
-        ) {
-            Ok(_) => (),
-            Err(e) => return Err(
-                ApplicationError::Forbidden(
-                    ErrorContent::Message(e.to_string())
-                )
-            )
-        };
-
         let mut validator_err_map: HashMap<String, String> = HashMap::new();
-        self.validator.validate_username(&data.username).unwrap_or_else(|e| {
-            validator_err_map.insert("username".to_string(), e.to_string());
+        
+        self.validator.validate_box_name(&data.box_name).unwrap_or_else(|e| {
+            validator_err_map.insert("box_name".to_string(), e.to_string());
         });
-
-        self.validator.validate_password(&data.password).unwrap_or_else(|e| {
-            validator_err_map.insert("password".to_string(), e.to_string());
-        });
-
-        self.validator.validate_email(&data.email).unwrap_or_else(|e| {
-            validator_err_map.insert("email".to_string(), e.to_string());
-        });
-
-        if let Some(first_name) = &data.first_name {
-            self.validator.validate_first_name(first_name).unwrap_or_else(|e| {
-                validator_err_map.insert("first_name".to_string(), e.to_string());
+        
+        if let Some(name) = &data.name {
+            self.validator.validate_object_name(name).unwrap_or_else(|e| {
+                validator_err_map.insert("name".to_string(), e.to_string());
             });
         }
-
-        if let Some(last_name) = &data.last_name {
-            self.validator.validate_last_name(last_name).unwrap_or_else(|e| {
-                validator_err_map.insert("last_name".to_string(), e.to_string());
+        
+        if let Some(path) = &data.path {
+            self.validator.validate_object_path(path).unwrap_or_else(|e| {
+                validator_err_map.insert("path".to_string(), e.to_string());
             });
         }
-
+        
+        // todo: validate metadata
+        
         if !validator_err_map.is_empty() {
             return Err(
                 ApplicationError::InvalidData(
@@ -94,87 +79,69 @@ impl Interactor<CreateUserDTO, CreateUserResultDTO> for CreateUser<'_> {
             )
         }
         
-        let default_role_id = match self.role_gateway.get_default_role().await {
-            Some(role) => role.id,
+        
+        let r#box = match self.box_reader.get_box_by_name_not_sensitive(&data.box_name).await {
+            Some(r#box) => r#box,
             None => {
-                return Err(ApplicationError::Forbidden(
-                    ErrorContent::Message(
-                        "Сервис на стадии инициализации. Роль по умолчанию не установлена!".to_string()
+                validator_err_map.insert("box_name".to_string(), "Box not found".to_string());
+                return Err(
+                    ApplicationError::InvalidData(
+                        ErrorContent::Map(validator_err_map)
                     )
-                ))
+                )
             }
         };
 
-        // Todo: to gather
-        let user_by_username = self.user_gateway.get_user_by_username_not_sensitive(&data.username).await;
-        let user_by_email = self.user_gateway.get_user_by_email_not_sensitive(&data.email).await;
-
-        // let (user_by_username, user_by_email) = match tokio::try_join!(
-        //     tokio::spawn(async move { self.user_gateway.get_user_by_username_not_sensitive(&data.username).await }),
-        //     tokio::spawn(async move { self.user_gateway.get_user_by_email_not_sensitive(&data.email).await })
-        // ) {
-        //     Ok((user_by_username, user_by_email)) => (user_by_username, user_by_email),
-        //     Err(e) => panic!("Error: {:?}", e)
-        // };
-        
-        if user_by_username.is_some() {
-            validator_err_map.insert("username".to_string(), "Имя пользователя занято".to_string());
-        }
-
-
-        if user_by_email.is_some() {
-            validator_err_map.insert("email".to_string(), "Пользователь с таким Email уже существует".to_string());
-        }
-
-
-        if !validator_err_map.is_empty() {
-            return Err(
-                ApplicationError::InvalidData(
-                    ErrorContent::Map(validator_err_map)
+        match self.access_service.ensure_can_create_object(
+            self.id_provider.is_auth(),
+            &r#box.id,
+            &self.id_provider.permissions()
+        ) {
+            Ok(_) => (),
+            Err(error) => return match error {
+                DomainError::AccessDenied => Err(
+                    ApplicationError::Forbidden(
+                        ErrorContent::Message(error.to_string())
+                    )
+                ),
+                DomainError::AuthorizationRequired => Err(
+                    ApplicationError::Unauthorized(
+                        ErrorContent::Message(error.to_string())
+                    )
                 )
-            )
-        }
-
-
-        let hashed_password = self.password_hasher.hash(&data.password).await;
-        
-        let user = self.user_service.create_user(
-            data.username,
-            data.email,
-            UserState::Inactive,
-            hashed_password,
-            data.first_name,
-            data.last_name,
-        );
-
-        self.user_gateway.save_user(&user).await;
-        self.role_gateway.link_role_to_user(&default_role_id, &user.id).await;
-        
-        let context: BTreeMap<String, Value> = {
-            let mut context = BTreeMap::new();
-            context.insert("username".to_string(), Value::String(user.username.to_string()));
-            context.insert("company".to_string(), Value::String(self.extra.company.to_string()));
-            context.insert("company_url".to_string(), Value::String(self.extra.company_url.to_string()));
-            context
+            }
         };
         
-        self.email_sender.send_template(
-            &user.email,
-            "Регистрация на сайте",
-            "registration.html",
-            Some(context),
-            13,
-            3600
-            
+        let object = self.object_service.create_object(
+            data.name,
+            data.path,
+            "".to_string(),
+            0,
+            "".to_string(),
+            data.metadata,
+        );
+
+        let file_hash = self.file_storage_writer.save_file(
+            object.id,
+            None,
+            None,
+            data.file
         ).await;
 
-        Ok(CreateUserResultDTO {
-            id: user.id,
-            username: user.username,
-            email: user.email,
-            state: user.state,
-            first_name: user.first_name,
-            last_name: user.last_name,
+        self.object_gateway.save_object(&object).await;
+        
+        // Todo: sync with other nodes in background
+
+        Ok(CreateObjectResultDTO {
+            id: object.id,
+            name: object.name,
+            path: object.path,
+            hash: object.hash,
+            size: object.size,
+            content_type: object.content_type,
+            metadata: object.metadata,
+            created_at: object.created_at,
+            updated_at: None,
         })
     }
 }

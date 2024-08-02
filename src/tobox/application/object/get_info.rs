75302c63f49225098a8dc94fa649371a6426e9c2
JKearnsl <boyarshinovn@gmail.com>
2024-08-02 06:25:23 +0300

    added get_info usecase

---
+++ src/tobox/application/object/get_info.rs
@@ -0,0 +1,83 @@
+use std::collections::HashMap;
+use chrono::{DateTime, Utc};
+use serde::Deserialize;
+
+use crate::application::common::exceptions::{ApplicationError, ErrorContent};
+use crate::application::common::file_storage_manager::FileStorageReader;
+use crate::application::common::id_provider::IdProvider;
+use crate::application::common::interactor::Interactor;
+use crate::application::common::object_gateway::ObjectReader;
+use crate::domain::exceptions::DomainError;
+use crate::domain::models::object::ObjectId;
+use crate::domain::models::r#box::BoxId;
+use crate::domain::services::access::AccessService;
+
+#[derive(Debug, Deserialize)]
+pub struct GetObjectInfoDTO {
+    pub id: ObjectId
+}
+
+#[derive(Debug, Deserialize)]
+pub struct GetObjectInfoResultDTO {
+    pub id: ObjectId,
+    pub name: String,
+    pub path: String,
+    pub hash: String,
+    pub size: u64,
+    pub content_type: String,
+    pub metadata: HashMap<String, String>,
+    pub box_id: BoxId,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: Option<DateTime<Utc>>
+}
+
+pub struct GetObjectInfo<'a> {  
+    pub object_reader: &'a dyn ObjectReader,
+    pub file_storage_reader: &'a dyn FileStorageReader,
+    pub access_service: &'a AccessService,
+    pub id_provider: Box<dyn IdProvider>
+}
+
+impl Interactor<GetObjectInfoDTO, GetObjectInfoResultDTO> for GetObjectInfo<'_> {
+    async fn execute(&self, data: GetObjectInfoDTO) -> Result<GetObjectInfoResultDTO, ApplicationError> {
+
+        let object = self.object_reader.get_object_by_id(&data.id).await.ok_or(
+            ApplicationError::NotFound(
+                ErrorContent::Message("Object not found".to_string())
+            )
+        )?;
+
+        match self.access_service.ensure_can_get_object(
+            self.id_provider.is_auth(),
+            &object.box_id,
+            self.id_provider.permissions()
+        ) {
+            Ok(_) => (),
+            Err(error) => return match error {
+                DomainError::AccessDenied => Err(
+                    ApplicationError::Forbidden(
+                        ErrorContent::Message(error.to_string())
+                    )
+                ),
+                DomainError::AuthorizationRequired => Err(
+                    ApplicationError::Unauthorized(
+                        ErrorContent::Message(error.to_string())
+                    )
+                )
+            }
+        };
+        
+        Ok(GetObjectInfoResultDTO {
+            id: object.id,
+            name: object.name.unwrap_or(object.id.to_string()),
+            path: object.path.unwrap_or("/".to_string()),
+            hash: object.hash,
+            size: object.size,
+            content_type: object.content_type,
+            metadata: object.metadata,
+            box_id: object.box_id,
+            created_at: object.created_at,
+            updated_at: object.updated_at
+        })
+    }
+}

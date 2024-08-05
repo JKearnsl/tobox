861780882375665179bdb034b6401b896e174a80
JKearnsl <boyarshinovn@gmail.com>
2024-08-05 11:08:59 +0300

    update: removed unused methods

--- src/tobox/application/common/id_provider.rs
+++ src/tobox/application/common/id_provider.rs
@@ -1,14 +1,8 @@
-use crate::domain::models::session::SessionId;
-use crate::domain::models::user::{UserId, UserState};
+use crate::domain::models::user::UserId;
 
-pub trait IdProvider: Send + Sync {
-    fn session_id(&self) -> Option<&SessionId>;
+pub trait IdProvider {
+    fn token(&self) -> Option<&String>;
     fn user_id(&self) -> Option<&UserId>;
-    fn user_state(&self) -> Option<&UserState>;
     fn permissions(&self) -> &Vec<String>;
-    fn client(&self) -> &str;
-    fn os(&self) -> &str;
-    fn device(&self) -> &str;
-    fn ip(&self) -> &str;
     fn is_auth(&self) -> &bool;
 }

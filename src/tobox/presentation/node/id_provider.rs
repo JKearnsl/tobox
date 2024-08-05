665d013268eef78a47a0af3b9007d657d7f8a3ca
JKearnsl <boyarshinovn@gmail.com>
2024-08-05 18:02:36 +0300

    update: impl make_token_provider

--- src/tobox/presentation/node/id_provider.rs
+++ src/tobox/presentation/node/id_provider.rs
@@ -1,55 +1,16 @@
 use actix_web::HttpRequest;
 
-use crate::adapters::auth::header::{HeaderPayload, IdHeaderProvider};
+use crate::adapters::auth::token::{IdTokenProvider, TokenProcessor};
+use crate::application::common::exceptions::{ApplicationError, ErrorContent};
 use crate::application::common::id_provider::IdProvider;
-use crate::domain::models::service::ServiceTextId;
 
-pub fn make_id_provider(
-    service_name: &ServiceTextId,
-    payload: Option<HeaderPayload>,
-    user_agent: Option<String>,
-    ip: &str
-) -> Box<dyn IdProvider> {
-    
-    Box::new(IdHeaderProvider::new(
-        service_name,
-        payload,
-        user_agent.unwrap_or("Unknown".to_string()),
-        ip
-    ))
-}
-
-pub fn make_id_provider_from_request(
-    service_name: &ServiceTextId,
-    is_intermediate: bool,
-    req: &HttpRequest
-) -> Box<dyn IdProvider> {
-    let headers = req.headers();
-    
-    let payload_raw = headers.get("payload").map(|value| {
-        value.to_str().unwrap().to_string()
-    });
-    let user_agent = headers.get("user-agent").map(|value| {
-        value.to_str().unwrap().to_string()
-    });
-    
-    let remote_addr = {
-        if is_intermediate {
-            req.connection_info().realip_remote_addr().unwrap().to_string()
-        } else {
-            req.connection_info().peer_addr().unwrap().to_string()
-        }
-    };
-
-    let payload: Option<HeaderPayload> = match payload_raw {
-        Some(payload_raw) => serde_json::from_str(&payload_raw).ok(),
-        None => None
-    };
-    
-    make_id_provider(
-        service_name,
-        payload,
-        user_agent,
-        remote_addr.as_str()
-    )
+pub fn make_token_provider(
+    req: &HttpRequest,
+    token_processor: &TokenProcessor
+) -> Result<Box<dyn IdProvider>, ApplicationError> {
+    let token = req.cookie("token").map(|cookie| cookie.value().to_string());
+    match IdTokenProvider::new(token, token_processor) {
+        Ok(provider) => Ok(Box::new(provider)),
+        Err(error) => Err(ApplicationError::Unauthorized(ErrorContent::from(error)))
+    }
 }

5a9447db2cff0c09974123fe28384692a99b9205
JKearnsl <boyarshinovn@gmail.com>
2024-08-05 18:23:48 +0300

    update: added PermissionTag

--- src/tobox/application/common/id_provider.rs
+++ src/tobox/application/common/id_provider.rs
@@ -1,8 +1,9 @@
+use crate::domain::models::permission::PermissionTag;
 use crate::domain::models::user::UserId;
 
 pub trait IdProvider {
     fn token(&self) -> Option<&String>;
     fn user_id(&self) -> Option<&UserId>;
-    fn permissions(&self) -> &Vec<String>;
+    fn permissions(&self) -> &Vec<PermissionTag>;
     fn is_auth(&self) -> &bool;
 }

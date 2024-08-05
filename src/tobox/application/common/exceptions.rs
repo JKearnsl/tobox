69fae23db0ec0fa497f4f5c35dacdf590da85dcf
JKearnsl <boyarshinovn@gmail.com>
2024-08-05 15:24:45 +0300

    update: added from traits; removed Into<String>

--- src/tobox/application/common/exceptions.rs
+++ src/tobox/application/common/exceptions.rs
@@ -4,14 +4,32 @@ use serde::Serialize;
 use crate::domain::exceptions::DomainError;
 
 #[derive(Debug, Serialize, Clone)]
-pub enum ErrorContent<T: Into<String> = String> {
-    Message(T),
+pub enum ErrorContent {
+    Message(String),
     Map(HashMap<String, String>),
 }
 
-impl From<DomainError> for ErrorContent<String> {
+impl From<DomainError> for ErrorContent {
     fn from(error: DomainError) -> Self {
         ErrorContent::Message(error.to_string())
+    }
+}
+
+impl From<&str> for ErrorContent {
+    fn from(s: &str) -> Self {
+        ErrorContent::Message(s.to_string())
+    }
+}
+
+impl From<String> for ErrorContent {
+    fn from(s: String) -> Self {
+        ErrorContent::Message(s)
+    }
+}
+
+impl From<HashMap<String, String>> for ErrorContent {
+    fn from(map: HashMap<String, String>) -> Self {
+        ErrorContent::Map(map)
     }
 }
 

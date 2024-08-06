1f596e4a4c26cb2d8896233ac8e355280a63a4fe
JKearnsl <boyarshinovn@gmail.com>
2024-08-06 20:31:40 +0300

    update: added DiskConfig; session_ttl; AsyncConfigManager

--- src/tobox/config.rs
+++ src/tobox/config.rs
@@ -2,6 +2,9 @@ use std::fs::{File, OpenOptions};
 use std::io::Read;
 use std::io::Write;
 use std::sync::RwLock;
+use tokio::sync::RwLock as AsyncRwLock;
+use tokio::fs::OpenOptions as AsyncOpenOptions;
+use tokio::io::AsyncWriteExt;
 use serde::{Deserialize, Serialize};
 
 
@@ -21,8 +24,7 @@ pub struct ClusterConfig {
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct DiskConfig {
-    pub mode: ClusterMode,
-    pub volumes: Vec<String>,
+    pub path: String,
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
@@ -44,9 +46,9 @@ pub struct NodeConfig {
     pub host: String,
     pub port: u16,
     pub workers: Option<usize>,
-    pub is_intermediate: bool,
     pub tls: Option<Tls>,
     pub disk: DiskConfig,
+    pub session_ttl: u32,
     pub cluster: Option<ClusterConfig>,
     pub credentials: Option<CredentialsConfig>
 }
@@ -78,10 +80,9 @@ impl Default for Config {
                 port: 3030,
                 tls: None,
                 workers: None,
-                is_intermediate: false,
+                session_ttl: 3600,
                 disk: DiskConfig {
-                    mode: ClusterMode::Combined,
-                    volumes: vec![]
+                    path: "path/to/mount/point".to_string()
                 },
                 cluster: None,
                 credentials: None
@@ -159,6 +160,86 @@ impl ConfigManager {
             Err(error) => return Err(format!("Failed to open {}: {}", self.path, error)),
         };
         if let Err(error) = writeln!(file, "{}", serde_yaml::to_string(&config).unwrap()) {
+            return Err(format!("Failed to write to {}: {}", self.path, error));
+        }
+        Ok(())
+    }
+}
+
+pub struct AsyncConfigManager {
+    config: AsyncRwLock<Config>,
+    path: String,
+}
+
+impl AsyncConfigManager {
+    pub async fn from_file<T: Into<String>>(config_path: T) -> Self {
+        let config_path = &config_path.into();
+
+        if !std::path::Path::new(config_path).exists() {
+            match OpenOptions::new().write(true).create_new(true).open(config_path) {
+                Ok(mut file) => {
+                    if let Err(error) = writeln!(file, "{}", serde_yaml::to_string(&Config::default()).unwrap()) {
+                        log::error!("Failed to write to {}: {}", config_path, error);
+                        std::process::exit(1);
+                    }
+                },
+                Err(error) => {
+                    log::error!("Failed to create {}: {}", config_path, error);
+                    std::process::exit(1);
+                },
+            };
+        }
+
+        let mut file = match File::open(config_path) {
+            Ok(file) => file,
+            Err(error) => {
+                log::error!("Failed to open {}: {}", config_path, error);
+                std::process::exit(1);
+            }
+        };
+        let mut contents = String::new();
+        match file.read_to_string(&mut contents) {
+            Ok(_) => (),
+            Err(error) => {
+                log::error!("Failed to read from {}: {}", config_path, error);
+                std::process::exit(1);
+            }
+        };
+
+        let config = match serde_yaml::from_str(&contents) {
+            Ok(config) => config,
+            Err(error) => {
+                log::error!("Failed to parse {}: {}", config_path, error);
+                std::process::exit(1);
+            },
+        };
+
+        Self {
+            config: AsyncRwLock::new(config),
+            path: config_path.to_string(),
+        }
+    }
+
+    pub async fn get(&self) -> Config {
+        self.config.read().await.clone()
+    }
+
+    pub async fn set(&self, config: Config) {
+        self.config.write().await.clone_from(&config);
+    }
+
+    pub async fn save(&self) -> Result<(), String> {
+        let config = self.config.read().await.clone();
+        let mut file = match AsyncOpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .open(&self.path)
+            .await 
+        {
+            Ok(file) => file,
+            Err(error) => return Err(format!("Failed to open {}: {}", self.path, error)),
+        };
+        if let Err(error) = file.write_all(serde_yaml::to_string(&config).unwrap().as_bytes()).await {
             return Err(format!("Failed to write to {}: {}", self.path, error));
         }
         Ok(())
